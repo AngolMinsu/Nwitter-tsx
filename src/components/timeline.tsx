23c5fc92dda9c427c28076b049404016bf4e92fe
Lobolic <steinsgate888@gmail.com>
2024-02-28 16:06:13 +0900

    using onSnapshot to query tweets

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,7 +1,15 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { db } from "../firebase";
+import { Unsubscribe } from "firebase/auth";
 import Tweet from "./tweet";
 
 export interface ITweet {
@@ -13,32 +21,59 @@ export interface ITweet {
   createdAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flext;
+  gap: 10px;
+  flect-direction: column;
+`;
+
 // firebase db를 쿼리함
 export default function Timeline() {
   const [tweets, setTweet] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(
-      collection(db, "tweets"),
-      orderBy("createAt", "desc")
-    );
-    const spanshot = await getDocs(tweetsQuery);
-    // 우리가 받은 모든 문서마다 이와 같은 객체를 만듬
-    const tweets = spanshot.docs.map((doc) => {
-      const { tweet, createdAt, userId, username, photo } = doc.data();
-      return {
-        tweet,
-        createdAt,
-        userId,
-        username,
-        photo,
-        id: doc.id,
-      };
-    });
-    setTweet(tweets);
-  };
+
+  // useEffect를 사용하여 함수를 호출함
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(db, "tweets"),
+        orderBy("createAt", "desc"),
+        limit(25) // pagenation
+      );
+      // 우리가 받은 모든 문서마다 이와 같은 객체를 만듬
+      //const spanshot = await getDocs(tweetsQuery);
+      // const tweets = spanshot.docs.map((doc) => {
+      //   const { tweet, createdAt, userId, username, photo } = doc.data();
+      //   return {
+      //     tweet,
+      //     createdAt,
+      //     userId,
+      //     username,
+      //     photo,
+      //     id: doc.id,
+      //   };
+      // });
+      // onSnapshot는 실시간으로 쿼리의 변경사항을 확인할 수 있음
+      // 또한 유저가 타임라인을 보고있을 때엔 비용을 지불하지 않도록 변수로 만듬
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweet(tweets);
+      });
+    };
     fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <Wrapper>
