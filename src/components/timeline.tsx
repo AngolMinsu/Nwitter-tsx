2974d14ff4653391329c2ad9e7d40a65ee29a61b
Lobolic <steinsgate888@gmail.com>
2024-02-28 15:47:08 +0900

    edit some errors

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -14,15 +14,16 @@ export interface ITweet {
 }
 
 const Wrapper = styled.div``;
-
+// firebase db를 쿼리함
 export default function Timeline() {
   const [tweets, setTweet] = useState<ITweet[]>([]);
   const fetchTweets = async () => {
     const tweetsQuery = query(
       collection(db, "tweets"),
-      orderBy("createdAt", "desc")
+      orderBy("createAt", "desc")
     );
     const spanshot = await getDocs(tweetsQuery);
+    // 우리가 받은 모든 문서마다 이와 같은 객체를 만듬
     const tweets = spanshot.docs.map((doc) => {
       const { tweet, createdAt, userId, username, photo } = doc.data();
       return {
